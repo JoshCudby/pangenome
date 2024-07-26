14264aa9ddd5cdb53444272aee923f7101ad0f00
Josh Cudby <jjcc2@cam.ac.uk>
2024-07-26 10:37:52 +0100

    Tidy up hacky weight assignment

--- tangle/max_path_dwave.py
+++ tangle/max_path_dwave.py
@@ -13,54 +13,6 @@ from utils.sampling_utils import print_path, validate_path
 if len(sys.argv) > 1:
     filename = sys.argv[1]
     graph = graph_from_gfa_file(f"data/{filename}")
-    
-    # # Real weight assignment
-    # for node in graph.nodes:
-    #     if re.search("^c\d*$", node):
-    #         graph.nodes[node]["weight"] = 1
-    #     elif re.search("^a1c\d*$", node):
-    #         graph.nodes[node]["weight"] = 1
-    #     elif re.search("^c6a1*$", node):
-    #         graph.nodes[node]["weight"] = 1
-    #     elif re.search("^cnv*$", node):
-    #         graph.nodes[node]["weight"] = 3
-    #     elif re.search("^cnvlink*$", node):
-    #         graph.nodes[node]["weight"] = 1
-    #     elif re.search("^cnvstr*$", node):
-    #         graph.nodes[node]["weight"] = 26
-    #     else:
-    #         graph.nodes[node]["weight"] = 0
-    
-    # graph.nodes['rCT']["weight"] = 20
-    # graph.nodes['rCA']["weight"] = 26
-    # graph.nodes['r16']["weight"] = 3
-    # graph.nodes['a1i0']["weight"] = 1
-    # graph.nodes['i5']["weight"] = 1
-    
-    try:
-        for node in graph.nodes:
-            graph.nodes[node]["weight"]
-    except KeyError:
-        # Arbitrary weight assignment
-        for node in graph.nodes:
-            if re.search(r"^c\d*$", node):
-                graph.nodes[node]["weight"] = 1
-            elif re.search("^r", node):
-                graph.nodes[node]["weight"] = 2
-            elif re.search(r"^a1c\d*$", node):
-                graph.nodes[node]["weight"] = 1
-            elif re.search("^c6a1*$", node):
-                graph.nodes[node]["weight"] = 1
-            elif re.search("^cnv*$", node):
-                graph.nodes[node]["weight"] = 3
-            elif re.search("^cnvlink*$", node):
-                graph.nodes[node]["weight"] = 1
-            elif re.search("^cnvstr*$", node):
-                graph.nodes[node]["weight"] = 5
-            elif re.search(r"i\d", node):
-                graph.nodes[node]["weight"] = 1 if uniform(0, 1) > 0.5 else 0
-            else:
-                graph.nodes[node]["weight"] = 0
 
 else:
     graph = toy_graph(exact_solution=False)
