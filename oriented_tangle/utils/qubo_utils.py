ab1f326f7e006f5df1e6c1bb051c05dbe24e69ad
Josh Cudby <jjcc2@cam.ac.uk>
2024-08-09 09:38:55 +0100

    Oriented tangle

---
+++ oriented_tangle/utils/qubo_utils.py
@@ -0,0 +1,85 @@
+import numpy as np
+import networkx as nx
+from itertools import product
+
+
+def qubo_matrix_from_graph(graph: nx.DiGraph) -> tuple[np.ndarray, float, int, int]:
+    """Constructs the QUBO matrix corresponding to a graph. Also returns the offset of the model, the max time and the number of nodes.
+
+    Args:
+        graph (nx.DiGraph): the node-weighted graph describing the problem.
+
+    Returns:
+        tuple[np.ndarray, float, int, int]: qubo_matrix, offset, T_max, V
+    """
+    nodes = list(graph.nodes)
+    V = int(len(nodes) / 2)
+    total_weight = int(sum(graph.nodes[node]["weight"] for node in nodes) / 2)
+    
+    # T_max = total weight + "a bit"
+    alpha = 1.2
+    T_max = int(total_weight * alpha)
+
+    # Penalty Values
+    lambda_t = 10 * T_max ** 2
+    lambda_g = T_max
+    lambda_end = T_max ** 2
+    lambda_w = T_max
+
+    qubo_matrix = np.zeros((T_max, V + 1, 2, T_max, V + 1, 2))
+    # Path constraint
+    for t in range(T_max):
+        for i in range(V):
+            for b in range(2):
+                qubo_matrix[t, i, b, t, i, b] -= lambda_t
+                qubo_matrix[t, V, 0, t, i, b] += 2 * lambda_t
+        qubo_matrix[t, V, 0, t, V, 0] -= lambda_t
+        
+        for i, j in product(range(V), range(V)):
+            for bi, bj in product(range(2), range(2)):
+                if not (i == j and bi == bj):
+                    qubo_matrix[t, i, bi, t, j, bj] += lambda_t
+        
+    # Graph edges constraint
+    for t in range(T_max - 1):
+        for i, j in product(range(V), range(V)):
+            for bi, bj in product(range(2), range(2)):
+                v0 = nodes[2 * i + bi]
+                v1 = nodes[2 * j + bj]
+                if (v0, v1) in graph.edges:
+                    qubo_matrix[t, i, bi, t+1, j, bj] -= lambda_g
+
+    # Steps to end are "free"
+    for t in range(T_max - 1):
+        for i in range(V):
+            for b in range(2):
+                qubo_matrix[t, i, b, t + 1, V, 0] -= (lambda_g - 1)
+        qubo_matrix[t, V, 0, t + 1, V, 0] -= (lambda_g - 1)
+        
+    # Leaving end is penalised
+    for t in range(T_max - 1):
+        for i in range(V):
+            for b in range(2):
+                qubo_matrix[t, V, 0, t + 1, i, b] += lambda_end
+                
+    # Weights constraints
+    for i in range(V):
+        for t in range(T_max):
+            for b in range(2):
+                qubo_matrix[t, i, b, t, i, b] -= (2 * graph.nodes[nodes[2 * i]]["weight"] - 1) * lambda_w
+        
+        for t1, t2 in product(range(T_max), range(T_max)):
+            for b1, b2 in product(range(2), range(2)):
+                if not (t1 == t2 and b1 == b2):
+                    qubo_matrix[t1, i, b1, t2, i, b2] += lambda_w
+
+    qubo_matrix = qubo_matrix.reshape((T_max * (V+1) * 2), (T_max * (V+1) * 2))
+    qubo_matrix = 0.5 * (qubo_matrix + qubo_matrix.T)
+
+    # Delete non-interacting rows and columns
+    qubo_matrix = np.delete(qubo_matrix, [t * (V+1) *2 + V * 2 + 1 for t in range(T_max)], 0)
+    qubo_matrix = np.delete(qubo_matrix, [t * (V+1) *2 + V * 2 + 1 for t in range(T_max)], 1)
+    
+    offset = lambda_t * T_max  + lambda_w * int(sum(graph.nodes[nodes[2 * i]]["weight"] ** 2 for i in range(V)))
+    
+    return qubo_matrix, offset, T_max, V
---
+++ oriented_tangle/utils/sampling_utils.py
@@ -0,0 +1,125 @@
+import numpy as np
+import networkx as nx
+import re
+from math import floor
+from dimod import BQM
+from dwave.system import LeapHybridSampler
+
+
+def dwave_sample_qubo(qubo_matrix: np.ndarray, offset: float, time_limit=None, label="Oriented QUBO") -> tuple[dict, float]:
+    """Perform a batch of annealing with greedy post-processing on a given Binary Quadratic Model.
+
+    Args:
+        sampler (Sampler): The sampler to anneal with.
+        bqm (BQM): The model to anneal.
+        time_limit (int, optional): The time limit.
+        label (str, optional): The label for sample submission on DWave platform.
+        
+    Returns:
+        (dict, float): Returns the best sample and best energy of the batch.
+    """
+    bqm = BQM(qubo_matrix, 'BINARY')
+    bqm.offset = offset
+    sampler = LeapHybridSampler()
+    if time_limit == -1:
+        time_limit = sampler.min_time_limit(bqm)
+        print(f"Using default min time limit: {time_limit}")
+    sampleset = sampler.sample(bqm, time_limit, label=label)
+    
+    try:
+        print(f"D-Wave access time: {round(sampleset.info['run_time'] / 10 ** 6)}")
+    except:
+        pass
+    
+    best_sample = sampleset.first.sample
+    best_energy = sampleset.first.energy
+    return best_sample, best_energy
+
+
+def sample_array_to_path(sample_array: np.ndarray, nodes: list, V: int):
+    nz = np.nonzero(sample_array == 1)
+    return [
+        (
+            nz[0][i], 
+            nodes[nz[1][i] * 2 + nz[2][i]] if nz[1][i] in range(V) else 'end'
+        ) for i in range(nz[0].shape[0])
+    ]
+
+
+def dwave_sample_to_path(graph: nx.Graph, sample: dict, T_max: int, V: int):
+    sample_list = np.array(list(sample.values()))
+    for idx in [t * (V + 1) * 2 + V * 2 + 1 for t in range(T_max)]:
+        sample_list = np.insert(sample_list, idx, 0)
+    sample_array = sample_list.reshape((T_max, V + 1, 2))
+    return sample_array_to_path(sample_array, list(graph.nodes), V)
+
+
+def sample_list_to_path(sample_list: np.ndarray, graph: nx.Graph, T_max: int, V: int):
+    for idx in [t * (V + 1) * 2 + V * 2 + 1 for t in range(T_max)]:
+        sample_list = np.insert(sample_list, idx, 0)
+    sample_array = sample_list.reshape((T_max, V + 1, 2))
+    return sample_array_to_path(sample_array, list(graph.nodes), V)
+    
+
+def print_path(path: list):
+    """Pretty print a path"""
+    num_per_line = 6
+    for i in range(floor(len(path) / num_per_line)):
+        print(path[i * num_per_line: (i + 1) * num_per_line])
+    if not (i + 1) * num_per_line == len(path):
+        print(path[(i + 1)*num_per_line:])
+        
+        
+def get_original_vertex_name(vertex_name):
+    pattern = r'(.+)_([\+\-])+'
+    match = re.search(pattern, vertex_name)
+    if match is None:
+        raise Exception('Could not retrieve vertex name')
+    else:
+        return match.group(1)
+        
+
+def validate_path(path: list, graph: nx.Graph):
+    """Checks the constraints for a path on a graph.
+    
+    In particular:
+     - does the path go along graph edges at each time step
+     - is each node visited the correct number of times
+     - is exactly one node visited per time step
+
+    Args:
+        path (list): _description_
+        graph (nx.Graph): _description_
+    """
+    print(f"Best path:")
+    print_path(path)
+    
+    time_offset = 0
+    for i in range(len(path)):
+        if i < path[i][0] + time_offset:
+            print(f'Skipped time {i}')
+            time_offset -= 1
+        elif i > path[i][0] + time_offset:
+            print(f'Visited 2 nodes at time {i}')
+            time_offset += 1
+    
+    node_dict = {node: 0 for node in graph.nodes}
+    node_dict['end'] = 0
+    
+    for x in range(len(path) - 1):
+        v1 = path[x][1]
+        node_dict[v1] += 1
+        v2 = path[x + 1][1]
+        if v1 == 'end' and not v2 == 'end':
+            print(f'Left the end node at path entry {x}')
+        elif (not v1 == 'end') and (not v2 == 'end') and (not (v1, v2) in graph.edges):
+            print(f'Broke graph edge at path entry {x}')
+    node_dict[v2] += 1
+    
+    nodes = list(graph.nodes)
+    for i in range(int(len(nodes) / 2)):
+        visits = node_dict[nodes[2 * i]] + node_dict[nodes[2 * i + 1]]
+        missing_visits = graph.nodes[nodes[2 * i]]["weight"] - visits
+        if  missing_visits != 0:
+            print(f'Did not meet node weight for node: {get_original_vertex_name(nodes[2 * i])}. Missing visits: {missing_visits}')
+
